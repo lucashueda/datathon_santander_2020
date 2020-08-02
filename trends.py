64b4dfdb8caa583c96ac15661cd14660d0ae9a42
Lucas Ueda <lucashueda@gmail.com>
2020-08-02 20:15:47 -0300

    fix trends.py

--- trends.py
+++ trends.py
@@ -12,7 +12,7 @@ def moving_average(a, n=3) :
 
 parser = argparse.ArgumentParser(description='Argparser for data preprocessing')
 
-parser.add_argument('--kw', nargs = '+', type = str, metavar = 'KW', 
+parser.add_argument('--kw', nargs = '+', metavar = 'KW', 
                     help = 'Lista das keywords a serem analisadas na trend')
 parser.add_argument('--me', default = '3', type = str, metavar = 'M', 
                     help = 'Meses de análise das trends')
